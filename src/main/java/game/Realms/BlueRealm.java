90160faa7499a325baac02976abf20ccf74b2a9f
Abdelrahman-Elabyad <randomalmno@gmail.com>
2024-04-07 22:15:19 +0200

    blue realm and serpent

---
+++ src/main/java/game/Realms/BlueRealm.java
@@ -0,0 +1,17 @@
+public class BlueRealm {
+    public static String getpossiblemoves(Dice dice){
+        //it uses a liked list serpents and returns the serpent.head as it is the only possible move
+    }
+    public static boolean checkdice(Dice dice){
+        //checks if the player is elligble to attak this realm [must be blue or white]
+        //also checks if the number he rolled is available for him to attack as if he rolled a 3 and
+        // the next creature needs to have a 4 then he is not allowed to attack
+    }
+    public static int getscore(){
+        // accumlates the number of heads defeated and gets the equivlent score from the table 
+        // it is also clled each round to show progress
+    }
+    public static boolean checkifmoveispossible(Dice dice){
+        // this can be applied seperatly or merged with checkdice 
+    }
+}
---
+++ src/main/java/game/creatures/Serpent.java
@@ -0,0 +1,17 @@
+public class Serpent {
+    protected linkedlist serpent ;
+public Serpent (int heads){
+    //constructor that creates a linked list of heads 
+}
+public static void attack(Dice dice){
+    //deletes the head from the linked list after being attacked
+}
+public static Boolean isserpentdead(){
+    //checks if serpent 1 is dead and calls the constructor to create a second serpent with six heads
+}
+public static string getbonus (Dice dice){
+    // switch function that  checks for the value of the dice if there is a corresponding award or bonus to be rewarded{calls the bonus function}
+    //also checks if it is attacking the first serpent or the second
+}
+
+}
