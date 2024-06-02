83b9d388ccaf9d903b16776002bde6e5c9e8a8de
Youssef Mostafa <youssefmostafa2372004@gmail.com>
2024-06-02 12:59:41 +0200

    yellow and Magenta

---
+++ src/main/java/game/gui/MagentaBonusController.java
@@ -0,0 +1,25 @@
+package game.gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+public class MagentaBonusController implements Initializable  {
+    @FXML private Button PhoenixButton;
+    @FXML private ImageView PhoenixImageView;
+    @FXML private ImageView BG;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image mainBG=new Image(Objects.requireNonNull(getClass().getResource("/images/MagentaRealmBG.jpg")).toExternalForm());
+        Image creature =new Image(Objects.requireNonNull(getClass().getResource("/images/MagentaPhoneix.png")).toExternalForm());
+    BG.setImage(mainBG);
+    PhoenixImageView.setImage(creature);
+    }
+    public void AttackPhoenix(ActionEvent event) {
+
+    }
+}
